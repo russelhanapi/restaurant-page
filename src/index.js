14b025347344c2b7b6a61737dae5e25fe89f0f9e
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-02-16 17:58:25 +0800

    feat: Implement tab switching functionality
    - Added id for tab buttons (Home, Menu, and Contact page)
    - Implemented event listeners for tab switching by clearing content before rendering the selected page.
    - Set Home page to load by default.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@ import { renderHomePage } from "./modules/home";
 import { renderMenuPage } from "./modules/menu";
 import { renderContactPage } from "./modules/contact";
 
-renderContactPage();
+const content = document.querySelector("#content");
+const homeTab = document.querySelector("#home-tab");
+const menuTab = document.querySelector("#menu-tab");
+const contactTab = document.querySelector("#contact-tab");
+
+homeTab.addEventListener("click", () => {
+  content.innerHTML = "";
+  renderHomePage();
+});
+
+menuTab.addEventListener("click", () => {
+  content.innerHTML = "";
+  renderMenuPage();
+});
+
+contactTab.addEventListener("click", () => {
+  content.innerHTML = "";
+  renderContactPage();
+});
+
+// Load Home Page by Default
+renderHomePage();
